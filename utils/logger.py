1ed357bd84f07a3e6f8920c521aa7625226aa0fc
Raghunandan <raghunandan092@gmail.com>
2025-03-19 18:13:30 -0400

    point to railway backend

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from typing import Any
 from pythonjsonlogger import jsonlogger
 
+
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record: dict[str, Any], record: logging.LogRecord, message_dict: dict[str, Any]) -> None:
         super().add_fields(log_record, record, message_dict)
